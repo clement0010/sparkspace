5db21320466b87e0a999feb3266854d6129200ca
Clement Tee <clement0010@hotmail.com>
2022-03-16 18:38:21 +0800

    feat: add inbox widget

---
+++ frontend/lib/inbox/inbox_widget.dart
@@ -0,0 +1,40 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InboxWidget extends StatefulWidget {
+  const InboxWidget({Key key}) : super(key: key);
+
+  @override
+  _InboxWidgetState createState() => _InboxWidgetState();
+}
+
+class _InboxWidgetState extends State<InboxWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+        automaticallyImplyLeading: false,
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
