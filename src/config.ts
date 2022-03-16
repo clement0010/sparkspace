fdebedd5b9aac330082725378de1c656dc66febd
Clement Tee <clement0010@hotmail.com>
2022-03-16 18:11:08 +0800

    fix: remove sensitive credentials in config file

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT ? process.env.PORT : 3000;
-export const SQL_PASSWORD = process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : 'Password123$';
-export const SQL_HOST = process.env.SQL_HOST ? process.env.SQL_HOST : '124.71.218.114';
+export const SQL_PASSWORD = process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : 'password';
+export const SQL_HOST = process.env.SQL_HOST ? process.env.SQL_HOST : 'localhost';
 export const HUAWEI_SK = process.env.HUAWEI_SK;
 export const HUAWEI_AK = process.env.HUAWEI_AK;
