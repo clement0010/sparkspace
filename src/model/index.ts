ce091e407d05f184f22632c3e589c6f42b00a56b
Clement Tee <clement0010@hotmail.com>
2022-03-16 17:51:51 +0800

    feat: add model entry file

---
+++ src/model/index.ts
@@ -0,0 +1,7 @@
+export * from './video';
+export * from './user';
+export * from './relationship';
+// export * from './tag';
+// export * from './like';
+// export * from './comment';
+// export * from './view';
